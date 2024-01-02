744cbd4c4d4758c34ba2b8a144374ab0455fbeaf
seritebridget <125568550+seritebridget@users.noreply.github.com>
2024-01-02 22:16:21 +0200

    added event listeners to my js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("Hello");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let seachFormElement = document.querySelector("#search-form");
+seachFormElement.addEventListener("submit", handleSearchSubmit);
