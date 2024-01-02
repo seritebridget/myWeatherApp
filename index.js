b26bbef035a47844722a516cde05c15d032e8dac
seritebridget <125568550+seritebridget@users.noreply.github.com>
2024-01-02 22:24:51 +0200

    changes to my js

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-function handleSearchSubmit(event) {
+function search(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
+
+  let apiKey = "0o1f34252a459ee87e614bt4fc070a90";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInputElement.value}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-let seachFormElement = document.querySelector("#search-form");
-seachFormElement.addEventListener("submit", handleSearchSubmit);
+function formatDate(date) {}
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector(".current-temperature-value");
+
+  // Assuming temperature is nested under data
+  let temperature = response.data.temperature.current;
+
+  temperatureElement.innerHTML = `${Math.round(temperature)}°C`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateElement = document.querySelector("#current-date");
+let currentDate = new Date();
+currentDateElement.innerHTML = formatDate(currentDate);
+
+let apiKey = "0o1f34252a459ee87e614bt4fc070a90";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Paris&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
